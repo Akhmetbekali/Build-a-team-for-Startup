f3699ee0ba446eafeac699a99706612513dfb79d
Apostrov <amir.subaev@gmail.com>
2019-03-18 23:11:22 +0300

    Integrate front with django

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html'))
+    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html')),
+    url(r'^registration/$', LoginView.as_view(template_name='accounts/registration.html')),
+    url(r'^profile/$', LoginView.as_view(template_name='accounts/profile.html')),
 ]
