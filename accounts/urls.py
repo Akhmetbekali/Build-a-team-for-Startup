77e55d7b0b5489597fefb84c96a4d613b9fd1297
Ali <akhmetbek.ali@gmail.com>
2019-03-26 11:16:22 +0300

    Reset password via email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,25 @@
 from django.conf.urls import url
 from accounts import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, \
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetView, PasswordResetCompleteView
+
 
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', LoginView.as_view(template_name='accounts/login.html')),
     url(r'^profile/logout/$', LogoutView.as_view(template_name='accounts/logout.html')),
     url(r'^registration/$', views.registration),
+    url(r'^change_password$', views.change_password),
+
+    url(r'^reset_password/$', PasswordResetView.as_view(template_name="registration/password_reset_form.html")),
+    url(r'^reset_password/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset_password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset_password/complete/$',
+        PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete'),
+
     url(r'^profile/$', views.profile),
     url(r'^profile/edit$', views.edit_profile),
-    url(r'^change_password$', views.change_password),
     url(r'^catalog/$', views.catalog),
 ]
