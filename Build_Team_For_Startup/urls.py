969135ec44467caf3142fca9799bb95f73bd2ac9
Kamil <kamileshik2700@gmail.com>
2019-03-30 17:31:14 +0300

    in profile(image)

--- Build_Team_For_Startup/urls.py
+++ Build_Team_For_Startup/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -24,3 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url('account/', include("accounts.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,8 @@ from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
     contact = models.CharField(max_length=20, default='')
     speciality = models.CharField(max_length=20, default='')
     description = models.CharField(max_length=100, default='')
@@ -18,3 +20,7 @@ def create_profile(sender, **kwargs):
 
 
 post_save.connect(create_profile, sender=User)
+
+
+def __str__(self):
+    return f'{self.user.username} UserProfile'
