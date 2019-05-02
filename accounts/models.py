d504b79c2697b83b6342998679a8bc38b856c087
Apostrov <amir.subaev@gmail.com>
2019-05-02 23:27:59 +0300

    Minor fixes

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,7 @@ class UserProfile(models.Model):
     speciality = models.CharField(max_length=20, default='')
     description = models.CharField(max_length=100, default='')
     image = models.ImageField(upload_to='profile_image', blank=True)
-    projects = models.ManyToManyField(ProjectPage, blank=True)
+    projects = models.ManyToManyField('ProjectPage', blank=True)
 
     def __str__(self):
         return f'{self.user.username} UserProfile'
